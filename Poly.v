200cd7a71d3024a708ce5759ec9e52f1e9456e41
zhaochaoyi <1251887477@qq.com>
2022-11-29 17:13:14 +0800

    Poly Left additional exercise

--- Poly.v
+++ Poly.v
@@ -30,12 +30,12 @@ Proof. reflexivity. Qed.
 Module MumbleGrumble.
 
 Inductive mumble : Type :=
-  | a : mumble
-  | b : mumble -> nat -> mumble
-  | c : mumble.
+    | a : mumble
+    | b : mumble -> nat -> mumble
+    | c : mumble.
 Inductive grumble (X:Type) : Type :=
-  | d : mumble -> grumble X
-  | e : X -> grumble X.
+    | d : mumble -> grumble X
+    | e : X -> grumble X.
 
 (** Which of the following are well-typed elements of [grumble X] for
     some type [X]?
@@ -172,3 +172,237 @@ Proof.
     - reflexivity.
     - simpl. rewrite rev_app_distr. rewrite IHL'. simpl. reflexivity.
 Qed.
+
+Inductive prod (X Y : Type) : Type :=
+| pair : X -> Y -> prod X Y.
+Arguments pair {X} {Y} _ _.
+
+Notation "( x , y )" := (pair x y).
+
+Notation " X * Y " := (prod X Y) : type_scope.
+
+(* 
+The annotation : type scope tells Coq that this abbreviation should only be used when
+parsing types. This avoids a clash with the multiplication symbol. 
+*)
+
+Definition fst {X Y : Type} (p : X * Y ) : X :=
+match p with
+| (x, y) => x
+end.
+
+Definition snd {X Y : Type} (p : X * Y ) : Y :=
+match p with
+| (x, y) => y
+end.
+
+Fixpoint combine {X Y : Type} (lx : list X ) (ly : list Y )
+: list (X * Y ) :=
+match lx, ly with
+| [], _ => []
+| _ , [] => []
+| x :: tx, y :: ty => (x, y) :: (combine tx ty)
+end.
+
+Check @combine.
+Compute (combine [1;2] [false;false;true;true]).
+
+Fixpoint split {X Y : Type} (l : list (X * Y )) : (list X ) * (list Y ) :=
+match l with
+| [] => ([] , [])
+| (x , y) :: l' => (x::(fst (split l')), y::(snd (split l')))
+end.
+
+Example test_split:
+split [(1,false);(2,false)] = ([1;2],[false;false]).
+Proof.
+reflexivity.
+Qed.
+
+Inductive option (X :Type) : Type :=
+| Some : X -> option X
+| None : option X.
+
+Arguments Some {X} _.
+Arguments None {X}.
+
+Fixpoint nth_error {X : Type} (l : list X ) (n : nat)
+: option X :=
+match l with
+| [] => None
+| a :: l' => if beq_nat n O then Some a else nth_error l' (pred n)
+end.
+
+Example testn_th_error1 : nth_error [4;5;6;7] 0 = Some 4.
+Proof. reflexivity. Qed.
+Example test_nth_error2 : nth_error [[1];[2]] 1 = Some [2].
+Proof. reflexivity. Qed.
+Example test_nth_error3 : nth_error [true] 2 = None.
+Proof. reflexivity. Qed.
+
+Definition hd_error {X : Type} (l : list X ) : option X :=
+match l with
+| [] => None
+| x :: l' => Some x
+end.
+Check @hd_error.
+Example test_hd_error1 : hd_error [1;2] = Some 1.
+Proof. reflexivity. Qed.
+Example test_hd_error2 : hd_error [[1];[2]] = Some [1].
+Proof. reflexivity. Qed.
+
+Definition doit3times {X :Type} (f :X -> X ) (n:X ) : X :=
+f (f (f n)).
+
+Check @doit3times.
+Example test_doit3times: doit3times minustwo 9 = 3.
+Proof. reflexivity. Qed.
+Example test_doit3times' : doit3times negb true = false.
+Proof. reflexivity. Qed.
+
+Fixpoint filter {X :Type} (test: X -> bool ) (l :list X )
+: (list X ) :=
+match l with
+| [] => []
+| h :: t => if test h then h :: (filter test t)
+            else filter test t
+end.
+
+Example test_filter1 : filter evenb [1;2;3;4] = [2;4].
+Proof. reflexivity. Qed.
+
+Definition length_is_1 {X : Type} (l : list X ) : bool :=
+beq_nat (length l) 1.
+Example test_filter2 :
+filter length_is_1
+[ [1; 2]; [3]; [4]; [5;6;7]; []; [8] ]
+= [ [3]; [4]; [8] ].
+Proof. reflexivity. Qed.
+
+Definition countoddmembers' (l :list nat) : nat :=
+length (filter oddb l ).
+Example test_countoddmembers'1 : countoddmembers' [1;0;3;1;4;5] = 4.
+Proof. reflexivity. Qed.
+Example test_countoddmembers'2 : countoddmembers' [0;2;4] = 0.
+Proof. reflexivity. Qed.
+Example test_countoddmembers'3 : countoddmembers' nil = 0.
+Proof. reflexivity. Qed.
+
+
+Example test_anon_fun' :
+doit3times (fun n => n * n) 2 = 256.
+Proof. reflexivity. Qed.
+
+Definition filter_even_gt7 (l : list nat) : list nat :=
+filter (fun x => if (blt_nat 7 x) && (evenb x) then true else false) l.
+
+Example test_filter_even_gt7_1 :
+filter_even_gt7 [1;2;6;9;10;3;12;8] = [10;12;8].
+Proof. simpl. reflexivity. Qed.
+Example test_filter_even_gt7_2 :
+filter_even_gt7 [5;2;6;19;129] = [].
+Proof. simpl. reflexivity. Qed.
+
+
+Definition partition {X : Type}
+(test : X -> bool )
+(l : list X )
+: list X * list X :=
+( (filter test l), (filter (fun x => negb (test x)) l)).
+
+Example test_partition1 : partition oddb [1;2;3;4;5] = ([1;3;5], [2;4]).
+Proof. simpl. reflexivity. Qed.
+
+Example test_partition2 : partition (fun x => false) [5;9;0] = ([], [5;9;0]).
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint map {X Y:Type} (f:X->Y) (l:list X)
+    : (list Y) :=
+match l with
+| [] => []
+| h :: t => (f h) :: (map f t)
+end.
+
+Example test_map1: map (plus 3) [2;0;2] = [5;3;5].
+Proof. reflexivity.  Qed.
+
+Example test_map2: map oddb [2;1;2;5] = [false;true;false;true].
+Proof. reflexivity.  Qed.
+
+Example test_map3:
+map (fun n => [evenb n;oddb n]) [2;1;2;5]
+= [[true;false];[false;true];[true;false];[false;true]].
+Proof. reflexivity.  Qed.
+
+Theorem map_common: forall (X Y : Type) (f : X -> Y ) (l1 l2 : list X ), map f (l1 ++ l2) = map f l1 ++ map f l2.
+Proof.
+    intros X Y f l1 l2.
+    induction l1 as [| h1 t1 IHl1'].
+    - reflexivity.
+    - simpl. rewrite IHl1'. reflexivity.
+Qed.
+
+Theorem map_rev : forall (X Y : Type) (f : X -> Y ) (l : list X ),
+map f (rev l ) = rev (map f l ).
+Proof.
+    intros X Y f l.
+    induction l as [|h t IHl'].
+    - reflexivity.
+    - simpl. rewrite map_common. simpl. rewrite IHl'. reflexivity.
+Qed.
+
+Fixpoint flat_map {X Y :Type} (f :X -> list Y ) (l :list X ): (list Y ) :=
+match l with
+| [] => []
+| h :: t => f h ++ flat_map f t
+end.
+
+Example test_flat_map1 :
+flat_map (fun n => [n;n;n]) [1;5;4]
+= [1; 1; 1; 5; 5; 5; 4; 4; 4].
+Proof. reflexivity.  Qed.
+
+Definition option_map {X Y : Type} (f : X -> Y ) (xo : option X )
+: option Y :=
+match xo with
+| None => None
+| Some x => Some (f x )
+end.
+
+Fixpoint fold {X Y :Type} (f : X -> Y -> Y ) (l :list X ) (b:Y )
+: Y :=
+match l with
+| nil => b
+| h :: t => f h (fold f t b)
+end.
+
+Check (fold andb).
+Example fold_example1 :
+fold mult [1;2;3;4] 1 = 24.
+Proof. reflexivity. Qed.
+Example fold_example2 :
+fold andb [true;true;false;true] true = false.
+Proof. reflexivity. Qed.
+Example fold_example3 :
+fold app [[1];[];[2;3];[4]] [] = [1;2;3;4].
+Proof. reflexivity. Qed.
+
+Definition constfun {X : Type} (x : X ) : nat -> X :=
+fun (k :nat) => x.
+Definition ftrue := constfun true.
+Example constfun_example1 : ftrue 0 = true.
+Proof. reflexivity. Qed.
+Example constfun_example2 : (constfun 5) 99 = 5.
+Proof. reflexivity. Qed.
+
+Check plus.
+
+Definition plus3 := plus 3.
+Check plus3.
+Example test_plus3 : plus3 4 = 7.
+Proof. reflexivity. Qed.
+Example test_plus3' : doit3times plus3 0 = 9.
+Proof. reflexivity. Qed.
+Example test_plus3'' : doit3times (plus 3) 0 = 9.
+Proof. reflexivity. Qed.
+
